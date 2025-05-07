d3455bd99286a3cf93d768096081bf4ed6f2f027
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-05-07 15:52:10 +0700

    basic usage zustand

---
+++ src/components/Bearbox/Bearbox.tsx
@@ -0,0 +1,20 @@
+import {useBearStore} from "../../zustand/bearStore.ts";
+
+export const Bearbox= () => {
+    // const bears = useBearStore(state => state.bears);
+    // const increasePopulation = useBearStore(state => state.increasePopulation);
+    // const removeAllBears = useBearStore(state => state.removeAllBears);
+
+    const {bears, increasePopulation, removeAllBears} = useBearStore();
+    return (
+        <div className='box'>
+            <h1>Bear Box</h1>
+            <p>bears: {bears}</p>
+            <div>
+                <button onClick={increasePopulation}>add bear</button>
+                <button onClick={removeAllBears}>remove bear</button>
+            </div>
+        </div>
+    );
+}
+
